1019a8b05a6523a74e0f28a347ab0f10d642e903
buffer38324 <20164493@stu.neu.edu.cn>
2019-04-04 20:16:03 +0800

    增加了一个DateTime类

--- OkrLibrary1/Models/OkrItem.cs
+++ OkrLibrary1/Models/OkrItem.cs
@@ -7,7 +7,9 @@ namespace OkrLibrary1.Models
     public class OkrItem
     {
         public int Level { get; set; }//任务的等级
+        public DateTime Time {get;set;}
         public string Content { get; set; }//内容
         public int Id { get; set; }//任务Id
     }
+
 }
