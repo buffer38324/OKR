f3aa16597c430b1e689c360f62752133bb8c8fb4
buffer38324 <20164493@stu.neu.edu.cn>
2019-04-04 19:49:02 +0800

    加了一个Model文件，：）

---
+++ OkrLibrary1/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OkrLibrary1
+{
+    public class Class1
+    {
+    }
+}
---
+++ OkrLibrary1/Models/OkrItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OkrLibrary1.Models
+{
+    public class OkrItem
+    {
+        public int Level { get; set; }//任务的等级
+        public string Content { get; set; }//内容
+        public int Id { get; set; }//任务Id
+    }
+}
