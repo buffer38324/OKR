e9b103e131e6236489b60f592de5d6d6e84ac499
cyy86 <20164458@stu.neu.edu.cn>
2019-04-07 16:22:17 +0800

    1.添加了首页界面 2.to Yuan:<TextBlock Name="LevelCoutText"/> 写在了等级那

--- Okr/MainPage.xaml.cs
+++ Okr/MainPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.DataTransfer;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -25,6 +26,83 @@ namespace Okr
         public MainPage()
         {
             this.InitializeComponent();
+            //数据共享
+            DataTransferManager dataTransferManager = DataTransferManager.GetForCurrentView();
+            dataTransferManager.DataRequested += DataTransferManager_DataRequested;
+        }
+
+        private void MyNavigation_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+
+        }
+
+        private void MyASB_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+            if (args.Reason == AutoSuggestionBoxTextChangeReason.UserInput)
+            {
+                //sender.ItemsSource = ReminderStrList.Where(i => i.Contains(sender.Text));
+            }
+        }
+
+        private void MyASB_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+            string txt = args.QueryText;  //输入的文本
+            if (args.ChosenSuggestion != null)
+            {
+                //从提示框中选择某一项时触发
+            }
+            else
+            {
+                //用户在输入时敲回车或者点击右边按钮确认输入时触发
+            }
+        }
+
+        private void MyASB_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+        {
+            sender.Text = args.SelectedItem.ToString();
+        }
+
+        private void ThemeButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ReviewButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void HelpButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CenterButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        /// <summary>
+        /// 启动共享UI
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ShareButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataTransferManager.ShowShareUI();
+        }
+
+        /// <summary>
+        /// 设置共享内容相关信息
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        private void DataTransferManager_DataRequested(DataTransferManager sender, DataRequestedEventArgs args)
+        {
+            DataRequest request = args.Request;
+            request.Data.SetText("Hello world!");    //共享文本或改成其他信息
+            request.Data.Properties.Title = "分享任务清单";
+            request.Data.Properties.Description = "分享任务清单，让小伙伴来监督你吧！ ";
         }
     }
 }
