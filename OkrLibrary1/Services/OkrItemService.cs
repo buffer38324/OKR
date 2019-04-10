8d7753f98e996d8a98be5214b8bcaad6320d8b95
buffer38324 <20164493@stu.neu.edu.cn>
2019-04-10 19:33:00 +0800

    添加Service内文件，初步写了保存函数，但是有点问题等明天研究完我再把剩下两个写完了

---
+++ OkrLibrary1/Services/OkrItemService.cs
@@ -0,0 +1,24 @@
+﻿using OkrLibrary1.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OkrLibrary1.Services
+{
+    /// <summary>
+    ///保存数据
+    /// </summary>
+    public class OkrItemService
+    {
+        public void AddOkrItems(int level, DateTime time, string content, int id)
+        {
+            OkrItem OkrItem1 = new OkrItem();
+            OkrItem1.Level = level;
+            OkrItem1.Time.Year = time.Year;
+            OkrItem1.Time.Month = time.Month;
+            OkrItem1.Time.Date = time.Date;
+            OkrItem1.Content = content;
+            OkrItem1.Id = id;
+        }
+    }
+}
