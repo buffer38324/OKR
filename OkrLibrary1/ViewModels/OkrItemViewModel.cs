749dd758e891986afa721fe01053e1f5197a1c72
66Yuan <m1123456799@163.com>
2019-05-01 22:22:29 +0800

    暂时去掉了OkrItem的Id属性

--- OkrLibrary1/ViewModels/OkrItemViewModel.cs
+++ OkrLibrary1/ViewModels/OkrItemViewModel.cs
@@ -10,9 +10,9 @@ namespace OkrLibrary1.ViewModels
         public List<OkrItem> AllOkrItems = new List<OkrItem>();
         
         //如果是叶子节点，调用此函数把Okr任务添加到List
-        public void AddOkrItems(int level, DateTimeOffset date, string title, int id,int need)
+        public void AddOkrItems(int level, DateTimeOffset date, string title,int need)
         {
-            AllOkrItems.Add(new OkrItem(level, date, title, id, need));
+            AllOkrItems.Add(new OkrItem(level, date, title, need));
         }
 
         //这两个函数需要改
